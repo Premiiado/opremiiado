70422a2c795c6806ebe8f3d183391b5b35dcb413
Premiiado <alexsnatos73748@gmail>
2021-02-01 19:21:34 -0300

    first commit

---
+++ settings/config.js
@@ -0,0 +1,4 @@
+const config = {
+        botName: 'Premiiado',
+        ownerName: 'Premiiado',
+}
---
+++ src/premiiadomenu.js
@@ -0,0 +1,12 @@
+const premiiadomenu = (prefix, pushname) => {
+    return `❍ *Comandos do Premiiado*
+    
+    ║➩ ❍ ${prefix}setprefix
+    ║➩ ❍ ${prefix}block
+    ║➩ ❍ ${prefix}tm
+    ║➩ ❍ ${prefix}tmctt
+    ║➩ ❍ ${prefix}clearall`
+
+}
+
+exports.premiiadomenu = premiiadomenu
